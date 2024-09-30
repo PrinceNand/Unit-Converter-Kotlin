eef3cd34d594d8ac6bea5d4815a95acd4e44324b
ashley <ashnand2000@gmail.com>
2024-09-30 23:12:55 +0530

    Kotlin-Learn: 3.Adding column and OutlinedTextField to enter value

--- app/src/main/java/com/project/unitconverterkotlin/MainActivity.kt
+++ app/src/main/java/com/project/unitconverterkotlin/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
             UnitConverterKotlinTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     UnitConverter(
-                        name = "Hey Android!",
+                        name = "Unit Converter!",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -33,7 +35,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter(name: String, modifier: Modifier){
-    Text(text = name)
+    Column {
+        Text(text = name)
+        OutlinedTextField(value = "", onValueChange = {})
+    }
 }
 
 @Preview(showBackground = true)
