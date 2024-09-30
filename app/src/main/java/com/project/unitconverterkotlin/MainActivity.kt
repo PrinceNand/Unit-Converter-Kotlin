6db2687dfd6b7f74ff3cb4c30612e108a431e292
ashley <ashnand2000@gmail.com>
2024-09-30 23:26:55 +0530

    Kotlin-Learn: 6.Modifier - verticalArrangement, horizontalAlignment

--- app/src/main/java/com/project/unitconverterkotlin/MainActivity.kt
+++ app/src/main/java/com/project/unitconverterkotlin/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,8 +18,8 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.project.unitconverterkotlin.ui.theme.UnitConverterKotlinTheme
@@ -42,7 +43,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter(name: String, modifier: Modifier){
-    Column {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(text = name)
         OutlinedTextField(value = "", onValueChange = {})
 
