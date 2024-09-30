70a119bfa747421e893eb095cd80100a77de0780
ashley <ashnand2000@gmail.com>
2024-09-30 23:07:00 +0530

    Kotlin-Learn: 2.Creating Composable and Preview

--- app/src/main/java/com/project/unitconverterkotlin/MainActivity.kt
+++ app/src/main/java/com/project/unitconverterkotlin/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.project.unitconverterkotlin.ui.theme.UnitConverterKotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +21,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             UnitConverterKotlinTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    UnitConverter(
+                        name = "Hey Android!",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +32,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun UnitConverter(name: String, modifier: Modifier){
+    Text(text = name)
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    UnitConverterKotlinTheme {
-        Greeting("Android")
-    }
-}
+fun UnitConverterPreview() {
+    UnitConverter(
+        name = "Hey Android!",
+        modifier = Modifier.padding(16.dp)
+    )}
